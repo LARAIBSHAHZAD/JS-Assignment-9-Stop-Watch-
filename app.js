d023668b08b4582653d9a6e23f2e7ae5f9235e1b
laraib shahzad <laraibshahzad9@gmail.com>
2023-10-15 13:07:48 +0500

    all files added

---
+++ app.js
@@ -0,0 +1,44 @@
+var mins = 0;
+var secs = 0;
+var milisecs = 0;
+
+var timer = false;
+
+function Start()
+{
+    timer = true;
+    stopWatch()
+}
+function Stop()
+{
+    timer = false;
+}
+function Reset()
+{
+    timer = false;
+    mins = 0;
+    secs = 0;
+    milisecs = 0;
+    document.getElementById("min").innerHTML = mins;
+    document.getElementById("secs").innerHTML = secs;
+    document.getElementById("milisec").innerHTML = milisecs;
+}
+function stopWatch()
+{
+    if(timer === true){
+        milisecs++;
+        if(milisecs === 100){
+            secs++;
+            milisecs = 0;
+        }
+        if(secs === 60){
+            mins++;
+            secs = 0;
+        }
+        document.getElementById("min").innerHTML = mins;
+        document.getElementById("secs").innerHTML = secs;
+        document.getElementById("milisec").innerHTML = milisecs;
+
+        setTimeout("stopWatch()", 10)
+    }
+}
